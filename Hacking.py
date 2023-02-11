21ce0c4f82537fac2acdc7834ae0541d67c18c15
Yousefnazeri <125121806+Yousefnazeri@users.noreply.github.com>
2023-02-11 16:30:17 +0330

    Update Hacking.py

--- Hacking.py
+++ Hacking.py
@@ -24,4 +24,4 @@ for i in l:
     sleep(1.5)
 print()
 
-print("code : 6283" or "9473" or "9272" or "8742" or "9753" or "4046")
+print("code : 6283" or "9473" or "9272" or "8742" or "9753" or "4046" )
